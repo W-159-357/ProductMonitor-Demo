25b96900e6e178463b9b74bc27b632dc618bc567
wxj <2574762712@qq.com>
2024-07-30 15:30:01 +0800

    add: Improvement of the jump to the detail page

--- ProductMonitor/ProductMonitor/MainWindow.xaml.cs
+++ ProductMonitor/ProductMonitor/MainWindow.xaml.cs
@@ -5,9 +5,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProductMonitor.OpCommand;
+using ProductMonitor.UserControls;
 using ProductMonitor.ViewModel;
 
 namespace ProductMonitor
@@ -17,12 +20,43 @@ namespace ProductMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainWindowVM mainWindowVM = new MainWindowVM();
+
         public MainWindow()
         {
             InitializeComponent();
 
             // 数据上下文实例化
-            this.DataContext = new MainWindowVM();
+            this.DataContext = mainWindowVM;
+        }
+
+        // 显示车间详情
+        private void ShowDetailUC()
+        {
+            WorkShopDetailUC workShopDetailUC = new WorkShopDetailUC();
+            mainWindowVM.MonitorUC = workShopDetailUC;
+
+            // 动画效果（由下而上）
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation(new Thickness(0, 50, 0, -50), new Thickness(0, 0, 0, 0), new TimeSpan(0, 0, 0, 0, 500));
+
+            // 透明度
+            DoubleAnimation doubleAnimation = new DoubleAnimation(0, 1, new TimeSpan(0, 0, 0, 0, 4000));
+
+            Storyboard.SetTarget(thicknessAnimation, workShopDetailUC);
+            Storyboard.SetTarget(doubleAnimation, workShopDetailUC);
+            Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Margin"));
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("Opacity"));
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(thicknessAnimation);
+            storyboard.Children.Add(doubleAnimation);
+            storyboard.Begin();
+        }
+
+        // 展示详情命令
+        public Command ShowDetailICmm
+        {
+            get { return new Command(ShowDetailUC); }
         }
     }
 }
---
+++ ProductMonitor/ProductMonitor/OpCommand/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProductMonitor.OpCommand
+{
+    public class Command : ICommand
+    {
+        private Action action;
+        public event EventHandler? CanExecuteChanged;
+
+        public Command(Action action)
+        {
+            this.action = action;
+        }
+
+        // 是否可以执行
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        // 执行方法
+        public void Execute(object? parameter)
+        {
+            if (action != null)
+            {
+                action();
+            }
+        }
+    }
+}
